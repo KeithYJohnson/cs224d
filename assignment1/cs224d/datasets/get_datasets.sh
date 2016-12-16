1a0c46bbb68458ef7d405c6eed9b10aa822dc090
Keith Johnson <keithyjohnson@gmail.com>
2016-12-16 14:49:44 -0500

    changing get_datasets to work for unix

--- assignment1/cs224d/datasets/get_datasets.sh
+++ assignment1/cs224d/datasets/get_datasets.sh
@@ -1,4 +1,4 @@
 # Get Stanford Sentiment Treebank
-wget http://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip
+curl -o ./stanfordSentimentTreebank.zip "http://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip"
 unzip stanfordSentimentTreebank.zip
 rm stanfordSentimentTreebank.zip
